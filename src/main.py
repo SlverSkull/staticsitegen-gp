014187b07deff06b44fc7a75cd3c3d4613a9758c
SlverSkull <legokiley@gmail.com>
2026-05-15 00:02:15 -0400

    Static Site Generator docs directory

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-import os, shutil
+import os, shutil, sys
 
 from textnode import TextNode, TextType
 from inline import markdown_to_html_node, extract_title
+
+basepath = "/"
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
 
 def copy_files(src, dst):
     if not os.path.exists(dst):
@@ -16,13 +20,13 @@ def copy_files(src, dst):
             copy_files(src_path, dst_path)
 
 def copy_static():
-    public = "public"
+    docs = "docs"
     static = "static"
-    if os.path.exists(public):
-        shutil.rmtree(public)
-    copy_files(static, public)
+    if os.path.exists(docs):
+        shutil.rmtree(docs)
+    copy_files(static, docs)
 
-def generate_page(src, dst, tmp):
+def generate_page(src, dst, tmp, bsp):
     print(f"Generating page from {src} to {dst} using {tmp}")
     with open(src) as f:
         src_read = f.read()
@@ -30,12 +34,12 @@ def generate_page(src, dst, tmp):
         tmp_read = f.read()
     html = markdown_to_html_node(src_read).to_html()
     title = extract_title(src_read)
-    replaced = tmp_read.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    replaced = tmp_read.replace("{{ Title }}", title).replace("{{ Content }}", html).replace('href="/', f'href="{bsp}').replace('src="/', f'src="{bsp}')
     os.makedirs(os.path.dirname(dst), exist_ok = True)
     with open(dst, "w") as f:
         f.write(replaced)
 
-def generate_pages(src, dst, tmp):
+def generate_pages(src, dst, tmp, bsp):
     if not os.path.exists(dst):
         os.mkdir(dst)
     
@@ -45,12 +49,12 @@ def generate_pages(src, dst, tmp):
         if os.path.isfile(src_path):
             if src_path.endswith(".md"):
                 dst_path = dst_path[:-3] + ".html"
-                generate_page(src_path, dst_path, tmp)
+                generate_page(src_path, dst_path, tmp, bsp)
         else:
-            generate_pages(src_path, dst_path, tmp)
+            generate_pages(src_path, dst_path, tmp, bsp)
 
 def main():
     copy_static()
-    generate_pages("content", "public", "template.html")
+    generate_pages("content", "docs", "template.html", basepath)
 
 main()
